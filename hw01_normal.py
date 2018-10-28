bf96692d55d75341123284c02768989ea0b97f4c
Alexandr Kurchevskiy <kurchevskiy.aleksandr@yandex.com>
2018-10-28 22:57:43 +0600

    add 3 task

--- hw01_normal.py
+++ hw01_normal.py
@@ -42,3 +42,18 @@ print(b, "Было Первым значением\n")
 # Для вычисления квадратного корня воспользуйтесь функцией sqrt() модуля math:
 # import math
 # math.sqrt(4) - вычисляет корень числа 4
+
+a = int(input("Введите значение a= "))
+b = int(input("Введите значение b= "))
+c = int(input("Введите значение c= "))
+D = b ** 2 - 4 * a * c
+if D < 0:
+    print("Корней нет")
+elif D == 0:
+    x = -b / 2 * a
+    print ("x = ", x)
+else:
+    x1 = (-b + math.sqrt(D)) / (2 * a)
+    x2 = (-b - math.sqrt(D)) / (2 * a)
+    print("x1 = ", x1)
+    print("x2 = ", x2)
